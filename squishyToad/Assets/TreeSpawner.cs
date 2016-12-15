8e61e4693da87bda252684508740da0f52ec4781
behram <batwrk@gmail.com>
2016-12-15 20:20:29 +0530

    UnityCardboard
    cardboard development

---
+++ squishyToad/Assets/TreeSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour {
+
+    public GameObject treePrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        ////CreateTree();
+        CreateTree();
+    }
+
+    void CreateTree()
+    {
+        var tree = Instantiate(treePrefab);
+        tree.transform.parent = transform;
+        tree.transform.localPosition = new Vector3(Random.Range(-50, 50),0,0);
+        //tree.transform.Translate(Random.Range(-50, 50), 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
